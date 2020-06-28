9629a4d510409ab8b1cc2411a8074593f4b23959
Adam Hutchin <ahutch@yahoo.com>
2020-06-28 16:57:41 -0700

    First attemp Schooners

--- js/jquery.tweet.js
+++ js/jquery.tweet.js
@@ -268,6 +268,6 @@
             username: "twitter",
             avatar_size: 50,
             count: 1,
-            loading_text: "loading tweets..."
+            loading_text: "Welcome."
         });
     });
